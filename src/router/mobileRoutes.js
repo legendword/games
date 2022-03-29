b7d9f22557c5cb4865f43e01d3b12bed55373b16
Legendword <zwh@legendword.com>
2022-03-28 19:56:03 -0700

    fix missing mobile route; filter 3-letter words

--- src/router/mobileRoutes.js
+++ src/router/mobileRoutes.js
@@ -5,7 +5,8 @@ const mobileRoutes = [
       children: [
         { path: '', component: () => import('pages/MobileIndex.vue'), meta: { showTabs: true } },
         { path: 'create', component: () => import('pages/MobileCreate.vue'), meta: { showTabs: true } },
-        { path: 'landlord/:roomId', component: () => import('src/pages/games/LandLord.vue') } //to be deleted
+        { path: 'landlord/:roomId', component: () => import('src/pages/games/LandLord.vue') }, //to be deleted
+        { path: 'wordle+', component: () => import('src/pages/games/WordlePlus.vue') }
       ]
     },
   
